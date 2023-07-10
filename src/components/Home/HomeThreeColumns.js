ca3dd5f8b474c5ff826300e746f9c0a37f1e0e70
EwelinaKopczewska <kopczewska.ewelina@gmail.comgit config --global user.name EwelinaKopczewskagit config --global user.email kopczewska.ewelina@gmail.com>
2023-07-10 11:47:13 +0200

    section three columns

--- src/components/Home/HomeThreeColumns.js
+++ src/components/Home/HomeThreeColumns.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const HomeThreeColumns = () => {
-    return <></>
+    return (
+    <div className="container_threecolumns">
+        <div className="threecolumns-box">
+            <p className="threecolumns_number">10</p>
+            <p className="threecolumns_title">ODDANYCH WORKÓW</p>
+            <p className="threecolumns_text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+        </div>
+        <div className="threecolumns-box">
+            <p className="threecolumns_number">5</p>
+            <p className="threecolumns_title">WSPARTYCH ORGANIZACJI</p>
+            <p className="threecolumns_text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+        </div>
+        <div className="threecolumns-box">
+            <p className="threecolumns_number">7</p>
+            <p className="threecolumns_title"> ZORGANIZOWANY ZBIÓREK</p>
+            <p className="threecolumns_text">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+        </div>
+    </div>
+    )
 }
 
 export default HomeThreeColumns
