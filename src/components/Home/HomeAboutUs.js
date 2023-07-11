a375962e3901ae068e1ba54b23fda1a18f908c72
EwelinaKopczewska <kopczewska.ewelina@gmail.comgit config --global user.name EwelinaKopczewskagit config --global user.email kopczewska.ewelina@gmail.com>
2023-07-11 11:43:33 +0200

    Section Home-AboutUs

--- src/components/Home/HomeAboutUs.js
+++ src/components/Home/HomeAboutUs.js
@@ -1,7 +1,20 @@
 import React from "react";
+import People from "../../assets/People.jpg"
+import Decoration from "../../assets/Decoration.svg"
+import Signature from "../../assets/Signature.svg"
 
 const HomeAboutUs = () => {
-    return <></>
+    return (
+    <div id="aboutUs" className="container_homeAboutUs">
+        <div className="homeAboutUs-box">
+            <p className = "homeAboutUs_title">O nas</p>
+            <img src={Decoration} className= "decoration" alt="Decoration"/>
+            <p className = "homeAboutUs_text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+            <img src={Signature} className= "signature" alt="Signarure"/>
+        </div>
+        <img src={People} className= "people_img" alt="People"/> 
+    </div>
+    )
 }
 
 export default HomeAboutUs
